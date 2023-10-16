2ed2b798d8781b30e3469345a49bdb37da86e521
Aliaksei <novik.alexey.phone@gmail.com>
2023-10-16 19:51:09 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int number1 = number/100;
+int number3 = number%10;
+int number4 = number1*10+number3;
+Console.Write(number4);
