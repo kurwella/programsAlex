188df8d6665f43d1ce16cd653b6cbec7938d03e6
Aliaksei <novik.alexey.phone@gmail.com>
2023-10-16 20:19:51 +0300

    task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿System.Console.WriteLine("Введите два числа в консоль: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int ostatok = number1%number2;
+if (number1%number2 == 0)
+{
+    System.Console.WriteLine("Кратно");
+}
+else
+{
+    System.Console.WriteLine($"Не кратно, остаток {ostatok}");
+}
