dcdf8ed45d092596f25583c5849eeab11e8cde51
Aliaksei <novik.alexey.phone@gmail.com>
2023-10-16 20:25:27 +0300

    Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите Число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    System.Console.WriteLine("Yes");
+}
+else
+{
+    System.Console.WriteLine("No");
+}
