2f289122ebb873150bd22770c58cca1e6eeea57e
Aliaksei <novik.alexey.phone@gmail.com>
2023-10-16 19:50:57 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = new Random().Next(10, 100);
+System.Console.WriteLine($"Ваше случайное число = {number}");
+int digit1 = number / 10;
+int digit2 = number % 10;
+
+if (digit1 > digit2)
+{
+    Console.WriteLine(digit1);
+}
+else if (digit1 < digit2)
+{
+    Console.WriteLine(digit2);
+}
+else
+{
+    Console.WriteLine("Цифры равны");
+}
