14ab71a8051bd1cb9c6c403ed605a4c51ad967fe
Aliaksei <novik.alexey.phone@gmail.com>
2023-10-17 12:32:31 +0300

    Fixed a program.

--- program_2/Program.cs
+++ program_2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random rnd = new Random();
+int guess = rnd.Next(1,11);
+
+System.Console.WriteLine("Угадайте число от 1 до 10");
+int guess_user = Convert.ToInt32(Console.ReadLine());
+
+if (guess == guess_user)
+{
+    System.Console.WriteLine("Число угадано!");
+}
+else
+{
+    System.Console.WriteLine($"Число не угадано :(, загаданное число - {guess}");
+}
