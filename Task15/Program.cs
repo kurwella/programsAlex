83389268d3ce99e594f6c5f31e86386b1a59d2d0
Aliaksei <novik.alexey.phone@gmail.com>
2023-10-20 00:11:07 +0200

    ZavodValidation

---
+++ Task15/Program.cs
@@ -0,0 +1,42 @@
+﻿int Prompt(string user_data)
+{
+    Console.Write(user_data);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool IsWeekend(int day)
+{
+    if (day > 5)
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
+
+bool Validate(int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Это не день недели");
+    return false;
+}
+
+int day = Prompt("Введите искомый день >>> ");
+if (Validate(day))
+{
+    if (IsWeekend(day))
+    {
+        Console.WriteLine("Отдыхай бро");
+    }
+    else
+    {
+        Console.WriteLine("На завод пиздуй");
+    }
+}
