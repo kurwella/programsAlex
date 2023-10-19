75684d6c22ab82665580c2683959f7dc0076467c
Aliaksei <novik.alexey.phone@gmail.com>
2023-10-20 00:29:11 +0200

    SecondRankAuth

---
+++ Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿int Prompt(string question)
+{
+    System.Console.WriteLine(question);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Prompt("Введите трехзначное число");
+if (number >= 1000 || number < 100)
+{
+    System.Console.WriteLine("Вы ввели не трехзначное число");
+    return;
+}
+System.Console.WriteLine($"Исходное число - {number}");
+int SecondNum = number / 10 % 10;
+System.Console.WriteLine($"Вторая цифра данного числа: {SecondNum}");
